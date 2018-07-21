343971cdebe9e1df24ab5738ad016057d1023395
Souvik Misra <svkmsr6@gmail.com>
2018-07-21 17:42:10 +0530

    Implementation of Upload Split and Download Merge

--- crumbs-middleware/server.js
+++ crumbs-middleware/server.js
@@ -2,8 +2,13 @@
 
 const Hapi = require('hapi');
 const sqlite3 = require('sqlite3').verbose();
-const node_dropbox = require('node-dropbox');
-const googleapis = require('googleapis');
+const fs = require('fs');
+//const node_dropbox = require('node-dropbox');
+const Dropbox = require('dropbox').Dropbox;
+const fetch = require('node-fetch');
+//const dropboxV2 = require('dropbox-v2-api');
+const splitter = require('split-file');
+//const googleapis = require('googleapis');
 
 const server = Hapi.server({
     port: 1990,
@@ -13,6 +18,13 @@ const server = Hapi.server({
 //For server testing
 let user = {};
 
+const db = new sqlite3.Database('../../crumbs_master', err => {
+	if (err) {
+		return console.error('Connection Error::',err.message);
+	}
+	console.log('Connected to the Crumbs SQlite database.');
+});
+
 //const closeDB = () => db.close();
 
 server.route({
@@ -20,12 +32,6 @@ server.route({
     path: '/users',
     handler: (request, h) => {
 		return new Promise(resolve => {
-			const db = new sqlite3.Database('../../crumbs_master', err => {
-				if (err) {
-					return console.error('Connection Error::',err.message);
-				}
-				console.log('Connected to the Crumbs SQlite database.');
-			});
 			let query = 'SELECT * FROM users';
 			db.all(
 			query, 
@@ -37,7 +43,7 @@ server.route({
 				const response = h.response(rows);
 				response.type('application/json');
 				response.header('Access-Control-Allow-Origin', '*');
-				db.close();
+				//db.close();
 				resolve(response);			
 			});	
 		});
@@ -49,12 +55,6 @@ server.route({
     path: '/users/{param}',
     handler: (request, h) => {
 		 return new Promise(resolve => {
-			const db = new sqlite3.Database('../../crumbs_master', err => {
-				if (err) {
-					return console.error('Connection Error::',err.message);
-				}
-				console.log('Connected to the Crumbs SQlite database.');
-			});
 			let query = 'SELECT * FROM users where user_id=? OR fname=?';
 			db.get(
 			   query, 
@@ -66,7 +66,7 @@ server.route({
 				const response = h.response(row);
 				response.type('application/json');
 				response.header('Access-Control-Allow-Origin', '*');
-				db.close();
+				//db.close();
 				resolve(response);			
 			});	
 		 });
@@ -78,12 +78,6 @@ server.route({
 		path: '/users/{param}/all',
 		handler: (request, h) => {
 			 return new Promise(resolve => {
-				const db = new sqlite3.Database('../../crumbs_master', err => {
-					if (err) {
-						return console.error('Connection Error::',err.message);
-					}
-					console.log('Connected to the Crumbs SQlite database.');
-				});
 				let query = `
 				select * from 
 				(select * from users u inner join subscriptions s on u.user_id = s.user_id ) t
@@ -100,7 +94,7 @@ server.route({
 					const response = h.response(row);
 					response.type('application/json');
 					response.header('Access-Control-Allow-Origin', '*');
-					db.close();
+					//db.close();
 					resolve(response);			
 				});	
 			 });
@@ -112,12 +106,6 @@ server.route({
 			path: '/groups/{param}/users',
 			handler: (request, h) => {
 				 return new Promise(resolve => {
-					const db = new sqlite3.Database('../../crumbs_master', err => {
-						if (err) {
-							return console.error('Connection Error::',err.message);
-						}
-						console.log('Connected to the Crumbs SQlite database.');
-					});
 					let query = `
 					select user_id, uname, fname, lname, user_pic,token from 
 					(select * from users u inner join subscriptions s on u.user_id = s.user_id ) t
@@ -134,7 +122,7 @@ server.route({
 						const response = h.response(rows);
 						response.type('application/json');
 						response.header('Access-Control-Allow-Origin', '*');
-						db.close();
+						//db.close();
 						resolve(response);			
 					});	
 				 });
@@ -142,57 +130,170 @@ server.route({
 			});
 
 			server.route({
+				method: 'GET',
+				path: '/files/{fileID}/chunks',
+				handler: (request, h) => {
+					 return new Promise(resolve => {
+						let query = `
+						select chunk_name from chunks where file_id=?
+						`;
+						db.all(
+						   query, 
+						   [request.params.fileID],
+						   (err, rows) => {
+							if (err) {
+								console.error('Query Error::',err.message);
+							}
+							const chunks = rows.map(row => row.chunk_name);
+							splitter.mergeFiles(chunks,'C:\\temp\\result.pdf')
+							.then(resp => {
+								console.log(resp);
+							})
+							// const response = h.response(rows);
+							// response.type('application/json');
+							// response.header('Access-Control-Allow-Origin', '*');
+							// db.close();
+							// resolve(response);			
+						});	
+					 });
+					}
+				});
+
+			server.route({
+				method: 'GET',
+				path: '/groups/{param}/files',
+				handler: (request, h) => {
+					 return new Promise(resolve => {
+						let query = `
+						select * from files where group_id=?
+						`;
+						db.all(
+						   query, 
+						   [request.params.param],
+						   (err, rows) => {
+							if (err) {
+								console.error('Query Error::',err.message);
+							}
+							const response = h.response(rows);
+							response.type('application/json');
+							response.header('Access-Control-Allow-Origin', '*');
+							//db.close();
+							resolve(response);			
+						});	
+					 });
+					}
+				});
+
+			server.route({
 				method: 'POST',
 				path: '/users/saveChunkInfo',
 				handler: (req, h) => {
 					 return new Promise(resolve => {
-						resolve(req.payload);
-						// const db = new sqlite3.Database('../../crumbs_master', err => {
-						// 	if (err) {
-						// 		return console.error('Connection Error::',err.message);
-						// 	}
-						// 	console.log('Connected to the Crumbs SQlite database.');
-						// });
-						// let insertQuery = 'update users set token = ? where user_id = ?';
-						// let params = [req.params.token, req.params.param];						
-						// db.run(insertQuery, params, function(err,data) {
-						// 	if (err) {
-						// 	  return console.error(err.message);
-						// 	}
-						// 	const response = h.response(data);
-						// 	response.type('application/json');
-						// 	response.header('Access-Control-Allow-Origin', '*');
-						// 	db.close();
-						// 	resolve(response);													   
-					    // });
-				});
-				}
-			});
-
-			server.route({
-				method: 'PUT',
-				path: '/users/{param}/saveToken/{token}',
-				handler: (request, h) => {
-					 return new Promise(resolve => {
-						const db = new sqlite3.Database('../../crumbs_master', err => {
-							if (err) {
-								return console.error('Connection Error::',err.message);
-							}
-							console.log('Connected to the Crumbs SQlite database.');
-						});
-						let insertQuery = 'update users set token = ? where user_id = ?';
-						let params = [req.params.token, req.params.param];
-						db.run(insertQuery, params, function(err,data) {
+						console.log(req.payload);
+						const fs = require('fs');
+						const payload = JSON.parse(req.payload);
+						const {file, fileURL, groupID, size, members} = payload;
+						let insertFilesQuery = 
+						'insert into files (group_id,file_name,file_type,file_size) values(?,?,?,?)';
+						let params = [groupID,file,'gen',size];						
+						db.run(insertFilesQuery, params, function(err) {
 							if (err) {
 							  return console.error(err.message);
 							}
-							const response = h.response(data);
-							response.type('application/json');
-							response.header('Access-Control-Allow-Origin', '*');
-							db.close();
-							resolve(response);													   
-					});
+							const fileID = this.lastID;
+							splitter.splitFile(fileURL, 3)
+								.then((names) => {
+									console.log('Split Success',names);
+									names.forEach((chunkPath,idx) => {
+										let token = members[idx].token;
+										let chunkContent = fs.readFileSync("C:\\temp\\"+chunkPath.split('\\').pop());
+										fetch('https://content.dropboxapi.com/2/files/upload',{
+											method: "POST",
+											headers: {
+											"Content-Type": "application/octet-stream",
+											"Authorization": "Bearer " + token,
+											"Dropbox-API-Arg":  "{\"path\": \"/temp/"+chunkPath.split('\\').pop()+"\",\"mode\": {\".tag\":\"overwrite\"},\"autorename\": true,\"mute\": false}",
+											},
+											body:chunkContent,
+											json: true
+										})
+										.then(resp => {
+											return resp.json();
+										})
+										.then(uploadResp =>{
+											console.log('Upload Success:::',uploadResp);
+											let insertChunksQuery = 
+											'insert into chunks (file_id, user_id, chunk_name) values(?,?,?)';
+											let chunkParams = [fileID, members[idx].user_id,chunkPath.split('\\').pop()];
+											db.run(insertChunksQuery, chunkParams, function(error) {
+												if(error){
+													return console.log('Chunk Data Persist Error:::', error);
+												}
+												const response = h.response({
+													status:'Chunking Success',
+													statusCode:'201'
+												});
+			 									response.type('application/json');
+			 									response.header('Access-Control-Allow-Origin', '*');
+												//db.close();
+								 				resolve(response);
+											});			
+										})
+										
+									});
+									//resolve(names);
+								})
+								.catch((err) => {
+									console.log('Error Splitting: ', err);
+									//resolve(err);
+								});				   
+					    	});
+						});
+					}
 				});
+
+			server.route({
+				method: 'GET',
+				path: '/groups/{groupID}/downloadFile/{fileID}',
+				handler: (req, h) => {
+					 return new Promise(resolve => {
+						let fetchQuery = `select user_id,chunk_name,token from (select * from users u inner join chunks c on u.user_id = c.user_id ) t where t.file_id=?`;
+						let params = [req.params.fileID];
+						const request = require('request');
+						db.all(fetchQuery, params, function(err,rows) {
+							if (err) {
+							  return console.error(err.message);
+							}
+							const mappedFiles = rows.map(row => (`C:\\temp\\${row.chunk_name}`)).sort();
+							rows.forEach((row,idx) => {
+								console.log('Downloading File:::',(idx+1));
+								const stream = request('https://content.dropboxapi.com/2/files/download',{
+									method: "POST",
+								 	headers: {
+								 	"Authorization": "Bearer " + row.token,
+								 	"Dropbox-API-Arg":  "{\"path\": \"/temp/"+row.chunk_name+"\"}"
+									}}
+								).pipe(fs.createWriteStream(`C:\\temp\\${row.chunk_name}`));
+								stream.on('finish',() => {
+									splitter.mergeFiles(mappedFiles, `C:\\temp\\merged_${row.chunk_name.split('.s')[0]}`)
+										.then(() => {
+											console.log('Downloaded '+row.chunk_name.split('.s')[0])
+										})
+										.catch((err) => {
+											console.log('Error: ', err);
+									});
+								});
+							});
+							const response = h.response({
+								status:'download',
+								code:'200'
+							});
+							 response.type('application/json');
+							 response.header('Access-Control-Allow-Origin', '*');
+							//db.close();
+							 resolve(response);										   
+						});
+					});
 				}
 			});
 
